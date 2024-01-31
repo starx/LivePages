6f0b0fc614722ee09292a8be60c670cfee9cab12
starx <swe_r3p0x9@mrnepal.com>
2024-01-31 10:50:46 +0000

    remove template code and add UserContent route

--- react_fe_main/src/App.js
+++ react_fe_main/src/App.js
@@ -5,8 +5,7 @@ import store from './store';
 import './App.css';
 import NoMatch from "./routes/NoMatch";
 import Home from "./routes/Home";
-import HelloWorld from "./routes/HelloWorld";
-import Users from "./routes/Users";
+import UserContent from "./routes/UserContent";
 
 function App() {
   return (
@@ -15,8 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="hello-world" element={<HelloWorld />} />
-            <Route path="users" element={<Users />} />
+            <Route path="user/:id" element={<UserContent />} />
 
             {/* Using path="*"" means "match anything", so this route
                   acts like a catch-all for URLs that we don't have explicit
@@ -35,28 +33,11 @@ function Layout() {
 
   return (
     <div>
-      <h1>Hello</h1>
-      <p>Welcome to React</p>
       <nav>
         <h2>Menu</h2>
         <ul>
           <li>
             <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/hello-world">Hello world</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-          <li>
-            <Link to={`${express_be_main_fqdn}`}>Express Home</Link>
-          </li>
-          <li>
-            <Link to={`${express_be_main_fqdn}/hello-world`}>Express Hello world</Link>
           </li>
         </ul>
       </nav>
